76ae47144ec93ed0c05d1af82464cf0de631ba27
Miseryset <1598654374@qq.com>
2024-01-04 19:30:53 +0800

    Update_to_7.1.1-25

--- ImageMagick-7.1.1-25/configs/arm64/MagickCore/version.h
+++ ImageMagick-7.1.1-25/configs/arm64/MagickCore/version.h
@@ -30,13 +30,13 @@ extern "C" {
 #define MagickLibVersion  0x711
 #define MagickLibVersionText  "7.1.1"
 #define MagickLibVersionNumber  10,0,1
-#define MagickLibAddendum  "'-24"
+#define MagickLibAddendum  "'-25"
 #define MagickLibInterface  10
 #define MagickLibMinInterface  10
 #define MagickPlatform  "aarch64"
 #define MagickppLibVersionText  "7.1.1"
 #define MagickppLibVersionNumber  5:0:0
-#define MagickppLibAddendum  "'-24"
+#define MagickppLibAddendum  "'-25"
 #define MagickppLibInterface  5
 #define MagickppLibMinInterface  5
 #define MagickReleaseDate  "2023-12-25"
